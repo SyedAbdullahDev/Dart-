a6d7508e641c42db00fbfe11c3bd037188a53f5b
SyedAbdullahDev <syed.abdullah.dev@gmail.com>
2024-07-09 17:50:43 +0500

    assignment

--- functions.dart
+++ functions.dart
@@ -1,28 +1,48 @@
- // functions    its a block of code that is used to perform specific task.    
+// functions    its a block of code that is used to perform specific task.
 //    1. No parameter and no return type , 2. Parameter and no return type ,
-// 3. no parameter and reutrn type , 4 Parameter and returntype.  
-
-//first of all  1. No parameter and no return type if theres nothing wrote in () it means 
+// 3. no parameter and reutrn type , 4 Parameter and returntype.
+//first of all  1. No parameter and no return type if theres nothing wrote in () it means
 //it has no parameter if theres void in start it means it has no return type.
-// anything that written in that() is parameter. like (n1 , n3)    
-                                     
-void printinfo(name , age ,degree){
-     print("Name : ${name}");
-     print("Age : ${age}");
-     print("degree : ${degree}");
-
- }
+// anything that written in that() is parameter. like (n1 , n3)
+// Best parctice for programing is to write function below the main function
+// Two types of parameter is formal and immformal parameter. Formal paramenter are which contain datatype and Immformal who not
 
 
-
-
-void main(){
- printinfo("Ahmad" , 19 , 'BSAI');
- printinfo("Abdullah" , 17 , 'BSCS');
-
-
-  
-}    //no parameter and no return type complete its all that . 
+// return_type - It can be any data type such as void, integer, float, etc. The return type must be matched with the returned value of the function.
+// func_name - It should be an appropriate and valid identifier.
+// parameter_list - It denotes the list of the parameters, which is necessary when we called a function.
+// return value - A function returns a value after complete its execution.
 
 
 
+void main() {
+  RoomSpace(300, 12000);
+}
+
+// Basic Reusable Function
+String calculator(int number1, int number2) {
+  print('Sum of n1 and n2 is ${number1 + number2}');
+  print('Sub of n1 and n2 is ${number1 - number2}');
+  print('Mul of n1 and n2 is ${number1 * number2}');
+  print('Div of n1 and n2 is ${number1 / number2}');
+  print('Reminder of n1 and n2 is ${number1 % number2}');
+  return 'Calculator';
+}
+
+int printinfo(String name, int age, String degree) {
+  print("Name : ${name}");
+  print("Age : ${age}");
+  print("degree : ${degree}");
+  return age;
+}
+
+RoomSpace(
+  double width,
+  double height,
+) {
+  print('My Room space is ${width * height}');
+}
+
+//1. Make A function that print exact time now.
+//2. Make a function that convert celcius to farhenheit
+//3. Make a function that get a int in paramenter and tell wheater its even or odd
